b73197638689d4f215198c98c67237ddaa488503
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-07 15:58:35 -0500

    fourth button was created

--- UIKit3-UIButtonUsingCode/ViewController.swift
+++ UIKit3-UIButtonUsingCode/ViewController.swift
@@ -54,11 +54,30 @@ class ViewController: UIViewController {
         return button
     }()
     
+    private lazy var swiftBetaButton4: UIButton = {
+        var configuration = UIButton.Configuration.bordered()
+        configuration.title = "Susbcribe to SwiftBeta"
+        configuration.titleAlignment = .center
+        configuration.subtitle = "Support the channel"
+        configuration.image = UIImage(systemName: "play.circle.fill")
+        configuration.imagePadding = 12
+        configuration.imagePlacement = .top
+        configuration.buttonSize = .large
+        
+        let button = UIButton(type: .system, primaryAction: UIAction(handler: { _ in
+            self.showMessage()
+        }))
+        button.configuration = configuration
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(swiftBetaButton1)
         view.addSubview(swiftBetaButton2)
         view.addSubview(swiftBetaButton3)
+        view.addSubview(swiftBetaButton4)
         
         NSLayoutConstraint.activate([
             swiftBetaButton1.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
@@ -68,11 +87,16 @@ class ViewController: UIViewController {
             swiftBetaButton2.topAnchor.constraint(equalTo: swiftBetaButton1.bottomAnchor, constant: 8),
             
             swiftBetaButton3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            swiftBetaButton3.topAnchor.constraint(equalTo: swiftBetaButton2.bottomAnchor, constant: 8)
+            swiftBetaButton3.topAnchor.constraint(equalTo: swiftBetaButton2.bottomAnchor, constant: 8),
+            
+            swiftBetaButton4.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            swiftBetaButton4.topAnchor.constraint(equalTo: swiftBetaButton3.bottomAnchor, constant: 8),
         ])
         // Do any additional setup after loading the view.
     }
 
-
+    func showMessage() {
+        print("Button tapped!")
+    }
 }
 
